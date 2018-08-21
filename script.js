ea3d780e901756a747d4311d850fcc6183fad7b8
rpedro10 <rpedro10@gmail.com>
2018-08-21 14:23:14 +0100

    become page done

--- script.js
+++ script.js
@@ -71,6 +71,36 @@ $(document).ready(function(){ //scroll in the homepage
   });
 });
 
+$(document).ready(function(){ //scroll in the homepage
+  // from: https://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
+
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: "#banner1", offset: 0});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#btn_more a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
+
+
 $(document).ready(function(){
   var form = document.getElementById("select-clinic");
   if(form){
